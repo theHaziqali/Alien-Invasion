c86be674df2968d9b49481b0bc534da42654e982
Haziq Ali <haziqaliofficial@gmail.com>
2021-08-03 23:44:09 +0500

    commit on main

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from Settings import Settings
+from AlienShip import Ship
 
 
 def run_game():
@@ -10,6 +11,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     #screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Alien Invasion")
+    # Make a ship
+    ship=Ship(screen)
+    
     # Set the background color.
     #bg_color = (170,40, 255)
     #screen.fill(bg_color)
@@ -17,8 +21,11 @@ def run_game():
     while True:
     # Watch for keyboard and mouse events.
        screen.fill(ai_settings.bg_color)
+      
+       ship.blitme()
        pygame.display.flip()
        for event in pygame.event.get():
             if event.type == pygame.QUIT:   
                 sys.exit()
+#----------------------------------MAIN--------------------------------------------------------------------------------#
 run_game()
